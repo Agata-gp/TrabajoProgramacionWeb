a8943b0c5292f91770fed10ddc014382709b803c
Agata <agambin7@alu.ucam.edu>
2026-05-30 12:30:13 +0200

    Cambio de directorio y añadiendo los php

---
+++ segunda_entrega/cabecera.php
@@ -0,0 +1,29 @@
+<header>
+    <section>
+        <h1>CyberLoot</h1>
+        <p class="slogan">Videojuegos, skins y contenido digital instantáneo</p>
+    </section>
+
+    <canvas id="logo" width="200" height="200"></canvas>
+</header>
+
+<nav>
+    <form class="search-bar" action="#" method="get">
+        <input type="text" name="buscar" placeholder="Buscar productos">
+        <button type="submit">Buscar</button>
+    </form>
+
+    <ul>
+        <li><a href="index.html">Inicio</a></li>
+        <li><a href="#">Videojuegos</a></li>
+        <li><a href="#">Tarjetas Regalo</a></li>
+        <li><a href="#">Skins</a></li>
+        <li><a href="#">Ofertas</a></li>
+        <li><a href="#">Carrito</a></li>
+        <li>
+            <a href="#" aria-label="Usuario">
+                <i class="fa-regular fa-circle-user fa-xl"></i>
+            </a>
+        </li>
+    </ul>
+</nav>
